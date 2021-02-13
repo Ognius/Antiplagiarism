cfea2f7b290ec064dc1254c270be3d6d7e100269
Ognius <ognius@tut.by>
2021-02-13 19:24:31 +0300

    update: serverRequest.js

--- src/serverRequest.js
+++ src/serverRequest.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", function () {
         let payload = 'name=' + encodeURIComponent(document.getElementById('value').value)
 
         const request = new XMLHttpRequest()
-        request.open('POST', 'http://34.86.152.212:8080/cgi-bin/script.cgi', true)
+        request.open('POST', 'http://35.236.72.195:8080/cgi-bin/script.cgi', true)
 
         request.addEventListener('readystatechange', function () {
             console.log(parseInt(request.responseText.replace(/[^\d]/g, '')));
