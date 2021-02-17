2868548631e39f93ab8b4c7e52df3c673acb6ee5
Ognius <ognius@tut.by>
2021-02-17 19:32:37 +0300

    update: serverRequest.js
    round up

--- src/serverRequest.js
+++ src/serverRequest.js
@@ -8,9 +8,10 @@ document.addEventListener("DOMContentLoaded", function () {
         request.open('POST', 'http://35.236.72.195:8080/cgi-bin/script.cgi', true)
 
         request.addEventListener('readystatechange', function () {
+            console.log(request.responseText);
             console.log(parseInt(request.responseText.replace(/[^\d]/g, '')));
             document.querySelector("#img").classList.add('opacity');
-            document.querySelector("#result").innerHTML = `${parseFloat(request.responseText.replace(/[^\d]/g, ''))} % unique`;
+            document.querySelector("#result").innerHTML = `${parseInt(request.responseText.replace(/[^\d]/g, ''))} % unique`;
         })
 
         request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded')
